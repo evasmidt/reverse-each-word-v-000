d36ef3bddec3e4a6f4d71eb9c15853b3ef418e17
Eva Smidt <shevangeline@gmail.com>
2018-02-10 22:08:04 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,12 +2,12 @@
 #Use the .collect method
 #Understand the return value of the .collect method
 #Use the return of collect for further operation
-def reverse_each_word(sentence = "Hello there, and how are you?")
-
-sentence2 = []
-sentence.each do |word|
+def reverse_each_word(sentence)
+sentence.split.collect {|word| word.reverse}.join(" ")
+#sentence2 = []
+#sentence.each do |word|
   #sentence.split
-  sentence2.reverse
+  #sentence2.reverse
 end
-sentence2
+#sentence2
 end
