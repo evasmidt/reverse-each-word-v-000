c0412334fe13617bd62057ed32c83529a3876352
Eva Smidt <shevangeline@gmail.com>
2018-02-10 21:59:37 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+#Understand the return value of the each method
+#Use the .collect method
+#Understand the return value of the .collect method
+#Use the return of collect for further operation
+def reverse_each_word(sentence = "Hello there, and how are you?")
+  sentence.each
+  sentence.split
+end
